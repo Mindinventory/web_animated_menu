f606ed0a2d5de01a2658f52d5bdf366d3602ce2b
Parth Mehta <parth.mehta@mindinventory.com>
2022-08-05 09:43:10 +0530

    - Refactoring code

--- lib/web_hover_menu.dart
+++ lib/web_hover_menu.dart
@@ -2,6 +2,6 @@ library web_hover_menu;
 
 export 'hover_menu/hover_animation_widget.dart';
 export 'utils/animation_type.dart';
-export 'model/header_model.dart';
-export 'model/menu_model.dart';
+export 'model/menu.dart';
+export 'model/sub_menu.dart';
 export 'utils/header_position.dart';
