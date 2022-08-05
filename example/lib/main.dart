73926b90c1bb903878efe34fbaf6097c9c7b7b90
Parth Mehta <parth.mehta@mindinventory.com>
2022-08-05 19:00:51 +0530

    - Improving GIFS

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -42,7 +42,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return AnimatedHoverMenu(
       headerPosition: HeaderPosition.bottomRight,
       headerTiles: headerModelList,
